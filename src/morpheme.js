c720be438ee7993f5a2b7b47bcc7d713e4b6ff90
immersedin <immersed_in_ideal@proton.me>
2025-02-27 18:49:30 +0300

    add some functional
    Co-authored-by: Valerii Bondariev <bdpwm@users.noreply.github.com>

---
+++ src/morpheme.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} str
+ * @param {string} suffix
+ * @return {string}
+ */
+export const addSuffix = (str, suffix) => `${str}${suffix}`;
+
+/**
+ * @param {string} str
+ * @param {string} prefix
+ * @return {string}
+ */
+export const addPrefix = (str, prefix) => `${prefix}${str}`;
--- src/pretty-num.js
+++ src/pretty-num.js
@@ -1,6 +1,7 @@
 import fromExponential from 'from-exponential';
 import thousands from 'thousands';
 import toPrecision from './to-precision.js';
+import { addSuffix } from './morpheme.js';
 
 /**
  * @import {PRECISION_SETTING} from './to-precision.js';
@@ -16,14 +17,39 @@ import toPrecision from './to-precision.js';
  * @param {string} [options.thousandsSeparator]
  * @param {string} [options.decimalSeparator = '.']
  * @param {boolean} [options.separateOneDigit = true]
+ * @param {string[]} [options.units]
+ * @param {number} [options.stepValue]
  * @return {string}
  */
-export default function prettyNum(value, {precision, precisionSetting, roundingMode, thousandsSeparator, decimalSeparator, separateOneDigit = true} = {}) {
+export default function prettyNum(value, {precision, precisionSetting, roundingMode, thousandsSeparator, decimalSeparator, separateOneDigit = true, units, stepValue} = {}) {
     // remove exponential notation
     let num = fromExponential(value);
 
     // reduce precision
     num = toPrecision(num, precision, {precisionSetting, roundingMode});
+
+    if (units && stepValue && Number(num) >= stepValue) {
+        let v = 1;
+        const temporaryNum = Number(num);
+        const objs = [];
+        const keys = Object.keys(objs);
+        let closeValue;
+
+        for (let index = 0, l = units.length; index < l; index += 1) {
+            v *= stepValue;
+            objs[v] = units[index];
+        }
+
+        const reducer = (a, c) => (Math.abs(Number(a) - temporaryNum) < Math.abs(Number(c) - temporaryNum) ? a : c);
+
+        for (let index = 0, l = keys.length; index < l; index += 1) {
+            const key = keys[index];
+            closeValue = reducer(closeValue, key);
+        }
+
+        const result = (temporaryNum / Number(closeValue)).toString();
+        return addSuffix(result, objs[closeValue]);
+    }
 
     // skip separation if `!separateOneDigit && num < 10000`
     if (thousandsSeparator && (separateOneDigit || Number(num) >= 10000)) {
